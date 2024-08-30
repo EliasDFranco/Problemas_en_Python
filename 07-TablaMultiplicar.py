e47e6f2c3e519fdc79f4db8465a5e080f6b1c0a1
Elias Franco Duarte <eliasfranco20160055@gmail.com>
2024-08-30 23:29:17 +0000

    Programa de Tabla de Multiplicar

---
+++ 07-TablaMultiplicar.py
@@ -0,0 +1,81 @@
+#Crea un programa que imprima la tabla de multiplicación del 1 al 10. 
+# El programa debe mostrar las tablas de multiplicar para cada número del 1 al 10.
+num1 = 1  
+num2 = 2  
+num3 = 3 
+num4 = 4 
+num5 = 5 
+num6 = 6
+num7 = 7
+num8 = 8
+num9 = 9
+num10 = 10
+resultado = 0
+
+print(f"Tabla del {num1}:") 
+for i in range(1, 11):
+    resultado = num1 * i
+    print(f"{num1} x {i} = {resultado}")
+    
+print(" ")
+
+print(f"Tabla del {num2}:")
+for i in range(1, 11):
+    resultado = num2 * i
+    print(f"{num2} x {i} = {resultado}")
+
+print(" ")
+
+print(f"Tabal del {num3}:")
+for i in range(1, 11):
+    resultado = num3 * i
+    print(f"{num3} x {i} = {resultado}")
+
+print(" ")
+
+print(f"Tabla del {num4}:")
+for i in range(1, 11):
+    resultado = num4 * i
+    print(f"{num4} x {i} = {resultado}")
+
+print(" ")
+
+print(f"Tabla del {num5}:")
+for i in range(1, 11):
+    resultado = num5 * i
+    print(f"{num5} x {i} = {resultado}")
+
+print(" ")
+
+print(f"Tabal del {num6}:")
+for i in range(1, 11):
+    resultado = num6 * i
+    print(f"{num6} x {i} = {resultado}")
+
+print(" ")
+
+print(f"Tabla del {num7}:")
+for i in range(1, 11):
+    resultado = num7 * i 
+    print(f"{num7} x {i} = {resultado}")
+    
+print(" ")
+
+print(f"Tabla del {num8}")
+for i in range(1, 11):
+    resultado = num8 * i
+    print(f"{num8} x {i} = {resultado}")
+
+print(" ")
+
+print(f"Tabla del {num9}")
+for i in range(1, 11):
+    resultado = num9 * i
+    print(f"{num8} x {i} = {resultado}")
+    
+print(" ")
+
+print(f"Tabla del {num10}")
+for i in range(1, 11):
+    resultado = num10 * i
+    print(f"{num10} x {i} = {resultado}")
