70c86a78112628b574fd4a7ced038f42cd2759ab
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-03-17 21:23:11 -0300

    Subiendo una calculadora con def

---
+++ 20-OperacionesBásicas.py
@@ -0,0 +1,19 @@
+def Suma(n1, n2):
+    return n1 + n2
+def Resta(n1, n2):
+    return n1 - n2
+def Multi(n1,n2):
+    return n1 * n2 
+def Divi(n1, n2):
+    return n1 / n2 
+def modulo(n1 , n2 ):
+    return n1 % n2
+
+n1 = 20
+n2 = 10
+
+print("La suma es: ", Suma(n1 , n2))
+print("La resta es: ", Resta(n1 , n2))
+print("La multiplicación es: ", Multi(n1 , n2))
+print("La división es: ", Divi(n1 , n2))
+print("El módulo es: ", modulo(n1 , n2))
