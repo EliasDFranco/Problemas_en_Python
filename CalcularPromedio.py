8541b5421b4d6185987d04e5787f46d6c48972b0
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-03-17 21:24:17 -0300

    Programa que calcula el promedio

---
+++ CalcularPromedio.py
@@ -0,0 +1,11 @@
+def calculadoraPromedio(nota1, nota2, nota3, nota4):
+    return (nota1 +  nota2 + nota3 + nota4 ) / 4 
+
+nota1 = 5
+nota2 = 4
+nota3 = 3 
+nota4 = 5
+
+respuestaPromedio = calculadoraPromedio(nota1, nota2, nota3, nota4)
+
+print("El promedio de estas cuatro notas es: ", respuestaPromedio)
