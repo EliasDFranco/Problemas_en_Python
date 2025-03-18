5142462423627b0ebaa48775f54d612f3f62b083
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-03-18 10:04:23 -0300

    Realizando una suma

---
+++ 24-SumaEjemplo.py
@@ -0,0 +1,9 @@
+nombre = input("Ingresar su nombre ")
+
+n1 = float(input("Ingresa su 1er número"))
+n2 = float(input("Ingresa su 2do número"))
+
+# Suma 
+suma = n1 + n2
+
+print(f"La suma de los números es: {suma}")
