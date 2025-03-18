646981527c89c66eec512b0c6d8b0299e5a1aa7e
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-03-17 21:22:12 -0300

    Subiendo Problema de una Función que suma

---
+++ 19-FunciónSuma.py
@@ -0,0 +1,11 @@
+def sumar(n1, n2, n3):
+    
+    return n1 + n2 + n3
+
+n1 = int(input("Ingrese el primer número: "))
+n2 = int(input("Ingrese el segundo : "))
+n3 = int(input("Ingrese el tercero número: "))
+    
+respuesta = sumar(n1,n2, n3)
+    
+print("El resultado de la suma es: ", respuesta)    
