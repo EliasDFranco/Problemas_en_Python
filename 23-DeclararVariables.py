543fa7a7a9a62dfc6a9f442147d800c26ffdbfad
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-03-17 21:28:44 -0300

    Declarar Variables

---
+++ 23-DeclararVariables.py
@@ -0,0 +1,5 @@
+nombreUsuario = input("Por favor, ingrese tu nombre")
+edadUsuario = input("Ingresa tu edad: ")
+alturaUsuario = float(input("Ingrese su altura: "))
+
+print(f"Hola, {nombreUsuario}, mi edad es: {edadUsuario}, y mi altura es: {alturaUsuario}")
